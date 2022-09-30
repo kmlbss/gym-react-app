dcd4392c9c22a71dc42e20635d4e388d431b80ce
kmlbss <49821804+kmlbss@users.noreply.github.com>
2022-09-30 20:57:42 +0400

    fetch data

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ export const exerciseOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': "f90ae19bacmshf12d13d29791ff3p1d68e4jsn25a6fc98acc7",
   },
 };
 
@@ -12,7 +12,7 @@ export const youtubeOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-    'X-RapidAPI-Key':process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key':"f90ae19bacmshf12d13d29791ff3p1d68e4jsn25a6fc98acc7",
   },
 };
 
